34a91ca32c781bfce2405723042fcf04578e10c6
vivien <vivienkorpys@outlook.fr>
2021-02-04 14:03:25 +0100

    Logout

--- EnquteSPA/MainWindow.xaml.cs
+++ EnquteSPA/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using MahApps.Metro.Controls.Dialogs;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,11 +25,17 @@ namespace EnquteSPA
     {
         public MainWindow()
         {
-            login frm = new login();
-            frm.ShowDialog();
+            //login frm = new login();
+            //frm.ShowDialog();
             InitializeComponent();
         }
-
+        protected override async void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            await this.ShowMessageAsync("Déconexion", "Vous allez bientôt quitter l'application.");
+            e.Cancel = false;
+            System.Windows.Application.Current.Shutdown();
+        }
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
           
