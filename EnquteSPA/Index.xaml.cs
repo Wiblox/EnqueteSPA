e3581484b5aba03921f67c009795bf7dc5dc0ce6
Théo Mielnikoff <theo.mielnikoff@gmail.com>
2021-05-02 20:33:51 +0200

    Création Index

---
+++ EnquteSPA/Index.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using EnquteSPA.bo;
+using System.Linq;
+
+namespace EnquteSPA
+{
+    /// <summary>
+    /// Logique d'interaction pour Index.xaml
+    /// </summary>
+    public partial class Index : UserControl
+    {
+        public Index()
+        {
+            InitializeComponent();
+            InitData();
+        }
+
+        public void InitData()
+        {
+            int nbEnquetesNonAssignees;
+            int nbEnquetesEnCours;
+            int nbEnqueteurs;
+
+            using (var db = new context())
+            {
+
+                nbEnquetesNonAssignees = db.Enquete.Where(o => o.Statut == (int)modele.StatutEnquete.NON_ASSIGNEE).Count();
+                nbEnquetesEnCours = db.Enquete.Where(o => o.Statut == (int)modele.StatutEnquete.EN_COURS).Count();
+                nbEnqueteurs = db.SpaPersonne.Where(o => o.DelegueEnqueteur == true).Count();
+            }
+
+            NbEnquetesNonAssignees.Text = nbEnquetesNonAssignees.ToString();
+            NbEnquetesEnCours.Text = nbEnquetesEnCours.ToString();
+            NbEnqueteurs.Text = nbEnqueteurs.ToString();
+        }
+    }
+}
---
+++ EnquteSPA/modele/StatutEnquete.cs
@@ -0,0 +1,10 @@
+﻿namespace EnquteSPA.modele
+{
+    enum StatutEnquete
+    {
+        NON_ASSIGNEE = 1,
+        EN_COURS = 2,
+        RENDUE = 3
+    }
+
+}
